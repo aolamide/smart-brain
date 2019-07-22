de03ad820ce9320961971142a8bbb2fb50240d64
Olamide <aboyejiolamide15@gmail.com>
2019-07-22 14:19:12 +0100

    Updates

---
+++ precache-manifest.ef56ce19cc988c035e0ad3de17ffffa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0eff21dfb82952397757",
+    "url": "/smart-brain/static/js/runtime~main.0eff21df.js"
+  },
+  {
+    "revision": "6680ab6e41ea676d5adf",
+    "url": "/smart-brain/static/js/main.6680ab6e.chunk.js"
+  },
+  {
+    "revision": "5345ee566ff9e5d4edf4",
+    "url": "/smart-brain/static/js/1.5345ee56.chunk.js"
+  },
+  {
+    "revision": "6680ab6e41ea676d5adf",
+    "url": "/smart-brain/static/css/main.6b278f1b.chunk.css"
+  },
+  {
+    "revision": "5345ee566ff9e5d4edf4",
+    "url": "/smart-brain/static/css/1.709bd38e.chunk.css"
+  },
+  {
+    "revision": "28b2584d2f0304722ee286f332c2d330",
+    "url": "/smart-brain/index.html"
+  }
+];
