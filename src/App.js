433a3a182891a2eb1f5aef3fa3d183ea05bf608b
Olamide <aboyejiolamide15@gmail.com>
2019-07-22 13:35:52 +0100

    fixed fetch url

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({imageUrl : this.state.input });
-  	fetch('http://localhost:3003/imageurl',{
+  	fetch('https://olasmartbrainapp.herokuapp.com//imageurl',{
       method : 'post',
       headers : {'Content-Type': 'application/json'},
       body : JSON.stringify({
@@ -89,7 +89,7 @@ class App extends Component {
     .then(res => res.json())
   	.then(response => {
       if(!response.err){
-        fetch('http://localhost:3003/image',{
+        fetch('https://olasmartbrainapp.herokuapp.com/image',{
           method : 'put',
           headers : {'Content-Type': 'application/json'},
           body : JSON.stringify({
